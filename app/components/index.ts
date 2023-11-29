f2a657eff146c2a26d21a03a89277d4d3dda930c
Nehorowo <trianglek4nz@gmail.com>
2023-11-29 17:52:43 +0200

    Create Link component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,6 @@
 import { ErrorMessage } from "./ErrorMessage";
 import { Spinner } from "./Spinner";
 import { IssueStatusBadge } from "./IssueStatusBadge";
+import { Link } from "./Link";
 
-export { ErrorMessage, Spinner, IssueStatusBadge };
+export { ErrorMessage, Spinner, IssueStatusBadge, Link };
