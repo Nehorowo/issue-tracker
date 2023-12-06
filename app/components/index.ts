4c52b0de15e79738aee3d1c5373acfa8409e3410
Pavel Berdnikov <p.berdnikov@softmg.ru>
2023-12-06 22:33:39 +0200

    Refactor Skeleton

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,5 +2,6 @@ import { ErrorMessage } from "./ErrorMessage";
 import { Spinner } from "./Spinner";
 import { IssueStatusBadge } from "./IssueStatusBadge";
 import { Link } from "./Link";
+import { Skeleton } from "./Skeleton";
 
-export { ErrorMessage, Spinner, IssueStatusBadge, Link };
+export { ErrorMessage, Spinner, IssueStatusBadge, Link, Skeleton };
