4f176aaf490740982e53bd0ec1ae03bdb8fd5e9f
Pavel Berdnikov <p.berdnikov@softmg.ru>
2023-12-28 21:52:25 +0200

    Refactor Issue page

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,19 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+
+export const IssueDetails = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Flex gap="3">
+        <Heading as="h1">{issue.title}</Heading>
+        <IssueStatusBadge status={issue.status} />
+      </Flex>
+      <Text>{issue.createdAt.toDateString()}</Text>
+      <Card className="prose mt-4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
