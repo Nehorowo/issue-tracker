832a32670ed42ce0210bb8be3b9d9a9529e526b8
Nehorowo <trianglek4nz@gmail.com>
2024-01-17 15:36:03 +0200

    Add dropdown menu for logout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,10 +4,17 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
-  const { status } = useSession();
+  const { status, data: session } = useSession();
   const path = usePathname();
   const links = [
     {
@@ -45,7 +52,24 @@ const NavBar = () => {
           <Box>
             <Box>
               {status === "authenticated" && (
-                <Link href="/api/auth/signout">Logout</Link>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <Avatar
+                      src={session.user!.image!}
+                      fallback="Avatar"
+                      size="2"
+                      radius="full"
+                    />
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                      <Text size="2">{session.user!.email}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                      <Link href="/api/auth/signout">Logout</Link>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
               )}
               {status === "unauthenticated" && (
                 <Link href="/api/auth/signin">Login</Link>
