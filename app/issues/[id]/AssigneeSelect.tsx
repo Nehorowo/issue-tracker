5a5a5aec6b7deae6673c2959e8342d0f2e0cc43b
Nehorowo <trianglek4nz@gmail.com>
2024-01-19 14:32:04 +0200

    Refactor AssigneeSelect

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,41 +1,34 @@
 "use client";
 
+import { Skeleton } from "@/app/components";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Skeleton } from "@/app/components";
 import toast, { Toaster } from "react-hot-toast";
 
 export const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users = [],
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, // 60 sec
-    retry: 3,
-  });
+  const { data: users = [], error, isLoading } = useGetUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
+  const handleAssignToUser = (userId: string) => {
+    axios
+      .patch(`/api/issues/${issue?.id}`, {
+        assignedToUserId: userId === "unassigned" ? null : userId,
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+  };
+
   return (
     <>
       <Select.Root
         defaultValue={issue?.assignedToUserId || "unassigned"}
-        onValueChange={async (userId) => {
-          axios
-            .patch(`/api/issues/${issue?.id}`, {
-              assignedToUserId: userId === "unassigned" ? null : userId,
-            })
-            .catch(() => {
-              toast.error("Changes could not be saved.");
-            });
-        }}
+        onValueChange={handleAssignToUser}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -54,3 +47,11 @@ export const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useGetUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, // 60 sec
+    retry: 3,
+  });
