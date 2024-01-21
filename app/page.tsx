9c853b9431792560580da6dbf06e4dd918474ada
Nehorowo <trianglek4nz@gmail.com>
2024-01-21 19:13:01 +0200

    Build an IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
+import prisma from "@/prisma/client";
+import { IssuesSummary } from "./IssuesSummary";
 import { LatestIssues } from "./LatestIssues";
-import { Pagination } from "./components";
+import { Flex } from "@radix-ui/themes";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
   return (
-    <div>
+    <Flex direction="column" gap="5">
       <LatestIssues />
-    </div>
+      <IssuesSummary open={open} inProgress={inProgress} closed={closed} />
+    </Flex>
   );
 }
